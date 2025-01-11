639f8f24080bb516f4cdd89125e3df98752fd624
Ondrej Zeman <ozeman77@gmail.com>
2025-01-11 17:39:11 +0100

    publishing to github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	basePath: "/new-home-site",
+	reactStrictMode: true,
+	swcMinify: true
 };
 
 module.exports = nextConfig;
