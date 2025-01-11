637bfc7b8d403568d996a0cb31be8951b5a10cd9
Ondrej Zeman <ozeman77@gmail.com>
2025-01-11 15:08:42 +0100

    finish nextjs rewrite

--- src/components/context/firebase.tsx
+++
@@ -1,6 +0,0 @@
-import { FirebaseApp } from 'firebase/app';
-import React from 'react';
-
-const FirebaseContext = React.createContext<FirebaseApp | undefined>(undefined);
-
-export { FirebaseContext };
--- src/service/firebase/firebase.ts
+++
@@ -1,6 +0,0 @@
-import { config } from './config';
-import { initializeApp, FirebaseApp } from 'firebase/app';
-
-const initFirebase = (): FirebaseApp => initializeApp(config);
-
-export default initFirebase;
